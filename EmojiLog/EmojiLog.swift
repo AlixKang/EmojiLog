9f3f526b9f7412ba676c4fbf04a4345d39b8f8e3
Alix.Kang <alix.kanglu@gmail.com>
2015-08-10 15:01:55 +0800

    Add __FUNCTION__ support

--- EmojiLog/EmojiLog.swift
+++ EmojiLog/EmojiLog.swift
@@ -10,34 +10,35 @@ import Foundation
 
 struct Log {
    
-    private static func log(emoji: String, content: String, file: AnyObject, line : AnyObject){
+    private static func log(emoji: String, content: String, file: AnyObject, line : AnyObject, function:AnyObject ) {
         let fileName = "\(file)".lastPathComponent
         let lineNumber = "\(line)"
-        let printContent =  emoji + "\t" + fileName + " " + lineNumber + " " + content
+        let functionName = "\(function)"
+        let printContent =  emoji + "\t" + fileName + " " + lineNumber +  " " + functionName + " " + content
         println("\(printContent)")
     }
     
     // VERBOSE
-    static func v(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__) {
-        log("↣", content: String(format: format, arguments: args), file: file, line:line)
+    static func v(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__, function:AnyObject = __FUNCTION__) {
+        log("↣", content: String(format: format, arguments: args), file: file, line:line, function: function)
     }
     // DEBUG
-    static func d(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__) {
-        log("✅", content: String(format: format, arguments: args), file: file, line:line)
+    static func d(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__, function:AnyObject = __FUNCTION__) {
+        log("✅", content: String(format: format, arguments: args), file: file, line:line, function: function)
     }
     // INFO
-    static func i(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__) {
-        log("ℹ️", content: String(format: format, arguments: args), file: file, line:line)
+    static func i(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__, function:AnyObject = __FUNCTION__) {
+        log("ℹ️", content: String(format: format, arguments: args), file: file, line:line, function: function)
     }
     
     // WARN
-    static func w(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__) {
-        log("⚠️", content: String(format: format, arguments: args), file: file, line:line)
+    static func w(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__, function:AnyObject = __FUNCTION__) {
+        log("⚠️", content: String(format: format, arguments: args), file: file, line:line, function: function)
     }
     
     // ERROR
-    static func e(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__) {
-        log("💔", content: String(format: format, arguments: args), file: file, line:line)
+    static func e(format: String, args: [CVarArgType], file: AnyObject = __FILE__, line: AnyObject = __LINE__, function:AnyObject = __FUNCTION__) {
+        log("💔", content: String(format: format, arguments: args), file: file, line:line, function: function)
     }
     
 }
